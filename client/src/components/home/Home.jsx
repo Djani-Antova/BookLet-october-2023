432a2adf460bc04b0def7ff2acbbc9bd6cd9ace0
djaniAntova <djaniantova@gmail.com>
2023-11-16 00:48:15 +0200

    Add Home component

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="hero_area">
+      <div className="hero_bg_box">
+        <div className="bg_img_box">
+          <img src="../../../public/images/homeBackgroun.PNG" alt="" />
+        </div>
+      </div>
+      {/* header section starts */}
+
+      {/* end header section */}
+
+      {/* slider section */}
+
+      {/* end slider section */}
+    </div>
+  );
+}
