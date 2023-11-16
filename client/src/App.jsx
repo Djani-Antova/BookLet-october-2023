407d0f832e74bec4833e1098a1de2f363298048a
djaniAntova <djaniantova@gmail.com>
2023-11-16 09:16:54 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import Footer from './components/footer/Footer'
+import Navigation from './components/navigation/Navigation'
+import Home from './components/home/Home'
 
 function App() { 
 
   return (
     <>
+      <Navigation />
+      <Home />
       <Footer />
     </>
   )
