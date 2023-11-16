bbb2e49e8b76bfc35e5a952dc33780232d129437
djaniAntova <djaniantova@gmail.com>
2023-11-16 21:40:41 +0200

    Update Footer component

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,112 +3,121 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="site-footer section">
+      <div className="container">
         <div className="section-container">
-            <div className="columns">
+          <div className="columns">
             <div className="newsletter">
-                <h5>BookLet</h5>
-                <p>
-                Join our newsletter to stay up yo date on our features and releases
-                </p>
-                <form action="#">
+              <h5>BookLet</h5>
+              <p>
+                Join our newsletter to stay up yo date on our features and
+                releases
+              </p>
+              <form action="#">
                 <input
-                    type="text"
-                    placeholder="Enter your email"
-                    name="newsletter-email"
-                    id="newsletter-email-footer"
+                  type="text"
+                  placeholder="Enter your email"
+                  name="newsletter-email"
+                  id="newsletter-email-footer"
+                  className="subscribe-input"
                 />
-                <button type="submit">Subscribe</button>
-                </form>
-                <p>
-                By subscribing you agree with our Privacy Policy and provide consent
-                to receive updates from our company.
-                </p>
+                <button type="submit" className="subscribe-button">
+                  Subscribe
+                </button>
+              </form>
+              <p>
+                By subscribing you agree with our Privacy Policy and provide
+                consent to receive updates from our company.
+              </p>
             </div>
             <div className="panel">
-                <h6>About us</h6>
-                <ul>
+              <h6>About us</h6>
+              <ul>
                 <li>
-                    <a href="">Popular</a>
+                  <a href="">Popular</a>
                 </li>
                 <li>
-                    <a href="">Trending</a>
+                  <a href="">Trending</a>
                 </li>
                 <li>
-                    <a href="">Contact</a>
+                  <a href="">Contact</a>
                 </li>
                 <li>
-                    <a href="">Support/Help</a>
+                  <a href="">Support/Help</a>
                 </li>
-        
-                </ul>
+              </ul>
             </div>
             <div className="panel">
-                <h6>More ...</h6>
-                <ul>
+              <h6>More ...</h6>
+              <ul>
                 <li>
-                    <a href="">Popular</a>
+                  <a href="">Popular</a>
                 </li>
                 <li>
-                    <a href="">Trending</a>
+                  <a href="">Trending</a>
                 </li>
                 <li>
-                    <a href="">Contact</a>
+                  <a href="">Contact</a>
                 </li>
                 <li>
-                    <a href="">Support/Help</a>
+                  <a href="">Support/Help</a>
                 </li>
-            
-                </ul>
+              </ul>
             </div>
             <div className="panel">
-                <h6>Follow us</h6>
-                <ul>
+              <h6>Follow us</h6>
+              <ul>
                 <li>
-                    <a href="#">
+                  <a href="#">
                     <i className="fab fa-facebook-f" />
                     <span>Facebook</span>
-                    </a>
+                  </a>
                 </li>
                 <li>
-                    <a href="#">
+                  <a href="#">
                     {" "}
                     <i className="fab fa-twitter" />
                     <span>Twitter</span>
-                    </a>
+                  </a>
                 </li>
                 <li>
-                    <a href="#">
+                  <a href="#">
                     {" "}
                     <i className="fab fa-instagram" />
                     <span>Instagram</span>
-                    </a>
+                  </a>
                 </li>
                 <li>
-                    <a href="#">
+                  <a href="#">
                     {" "}
                     <i className="fab fa-tiktok" />
                     <span>Tik Tok</span>
-                    </a>
+                  </a>
                 </li>
-                </ul>
+              </ul>
             </div>
-            </div>
-            <div className="info">
+          </div>
+          <div className="info">
             <p className="copyright">
-                2023 © <strong><a href="https://github.com/Djani-Antova">BookLet</a></strong> - All rights reserved.
+              2023 ©{" "}
+              <strong>
+                <a href="https://github.com/Djani-Antova">BookLet</a>
+              </strong>{" "}
+              - All rights reserved.
             </p>
             <ul>
-                <li>
+              <li>
                 <a href="#">Privacy Policy</a>
-                </li>
-                <li>
+              </li>
+              <li>
                 <a href="#">Terms of service</a>
-                </li>
-                <li>
+              </li>
+              <li>
                 <a href="#">Cookie Settings</a>
-                </li>
+              </li>
             </ul>
-            </div>
+          </div>
         </div>
-    </footer> 
-  )}  
+      </div>
+    </footer>
+  );
+}
