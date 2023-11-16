6c4a770a7df015c1b4d55b70e59d5861d14f2327
djaniAntova <djaniantova@gmail.com>
2023-11-16 09:16:46 +0200

    Update Navigation component and styling

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,39 +1,47 @@
 import React from "react";
 import "./Navigation.css"; // Import the CSS file
+import Logo from "/images/booklet-high-resolution-logo-white-transparent.png";
 
 const Navigation = () => {
   // const { user } = UseAuth();
 
   return (
     <header>
-      <div className="container">
-        <p>
-          <a className="navbar-brand" href="index.html">
-            <span>BookLet</span>
-          </a>
-        </p>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Nominations</a>
-            </li>
-            <li>
-              <a href="#">Trending</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a className="nav-link" id="login" href="#">
-                <i className="fa fa-user" /> Login
-              </a>
-            </li>
-            <button className="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-              <i className="fa fa-search"  />
-            </button>
-          </ul>
-        </nav>
-      </div>
+        <div className="container">
+            <p>
+                <a className="navbar-brand" href="index.html">
+                    <img src={Logo} alt="BookLet Logo" />
+                </a>
+            </p>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Nominations</a>
+                    </li>
+                    <li>
+                        <a href="#">Trending</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a className="nav-link" id="login" href="#">
+                            <i className="fa fa-user" /> Login
+                        </a>
+                    </li>
+                    <li>
+                        <a className="nav-link" id="register" href="#">
+                        <i class="fa-solid fa-right-to-bracket"></i> Register
+                        
+                        </a>
+                    </li>
+
+                    <button className="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                        <i className="fa fa-search" />
+                    </button>
+                </ul>
+            </nav>
+        </div>
     </header>
   );
 };
