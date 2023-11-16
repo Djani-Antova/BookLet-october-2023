21bb84d0f303c09cfbeae52851f10ec0c72adf8a
djaniAntova <djaniantova@gmail.com>
2023-11-16 11:41:51 +0200

    Add Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
+import { Routes, Route} from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navigation from './components/navigation/Navigation'
 import Home from './components/home/Home'
+import FeaturedList from './components/book-list/FeaturedList'
 
 function App() { 
 
-  return (
-    <>
-      <Navigation />
-      <Home />
-      <Footer />
-    </>
-  )
+    return (
+        <div id="box">
+            <Navigation />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/books" element={<FeaturedList />} />
+           
+            </Routes>
+            <Footer />
+        </div>
+    )
 }
 
 export default App
