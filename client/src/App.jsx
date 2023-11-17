4169d861f7e83d1d5e716b47f108721d9168c1f7
djaniAntova <djaniantova@gmail.com>
2023-11-17 15:11:23 +0200

    Add Routes to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,29 @@
-import { Routes, Route} from 'react-router-dom'
-import Navigation from './components/navigation/Navigation'
-import Footer from './components/footer/Footer'
-import Home from './components/home/Home'
-import FeaturedList from './components/book-list/FeaturedList'
-import CreateBookModal from './components/book-create/CreateBookModal'
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/navigation/Navigation";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import FeaturedList from "./components/book-list/FeaturedList";
+import CreateBookModal from "./components/book-create/CreateBookModal";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 
-function App() { 
+function App() {
+  return (
+    <div id="box">
+        <Navigation />
 
-    return (
-        <div id="box">
-            <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<FeaturedList />} />
+          <Route path="/create" element={<CreateBookModal />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/books" element={<FeaturedList />} />
-                <Route path="/create" element={< CreateBookModal/>} />              
-           
-            </Routes>
-
-            <Footer />
-        </div>
-    )
+        <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
