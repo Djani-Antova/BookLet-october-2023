9c0d8d74bdce70e5fac375e0734745af83ca4a0d
djaniAntova <djaniantova@gmail.com>
2023-11-18 11:21:59 +0200

    Update CreateBookModal.jsx

--- client/src/components/book-create/CreateBookModal.jsx
+++ client/src/components/book-create/CreateBookModal.jsx
@@ -1,77 +1,83 @@
 import "./CreateBookModal.css";
+import { useNavigate } from 'react-router-dom'
+import * as bookService  from '../../services/bookService';
 
-export default function CreateBookModal(
-  {
-    // onClose,
-    // onCreate,
-  }
-) {
-  return (
-    <div className="create-book-modal">
-      <div className="book-container">
-        <header className="headers">
-        <button
-              className="btn close"
-              // onClick={onClose}
-            >
-              X{/* <FontAwesomeIcon icon={faTimes} /> */}
-            </button>
-          <h2>
-            Nominate New Book
+export default function CreateBookModal() {
+    const navigate = useNavigate();
 
-          </h2>
+    const createBookSubmitHandler = async (e) =>{
+        e.preventDefault();
 
-          <form
-          // onSubmit={onCreate}
-          >
-            <label htmlFor="title">Title:</label>
-            <input type="text" id="title" name="title" />
-            {/* <p className="form-error">
-                            Title should be at least 3 characters long!
-                            </p> */}
+        const bookData = Object.fromEntries(new FormData(e.currentTarget));
 
-            <label htmlFor="author">Author:</label>
-            <input type="text" id="author" name="author" />
-            {/* <p className="form-error">
-                            Author should be at least 3 characters long!
-                            </p> */}
+        try {
+            await bookService.create(bookData);
+    
+            navigate('/books');            
+        } catch (error) {
+            //TODO Error notification => may use f.ex. Toaster message that error occured
+            console.log(error);
+        }
+    } 
 
-            <label htmlFor="imageUrl">Image URL:</label>
-            <input type="text" id="imageUrl" name="imageUrl" />
-            {/* <p className="form-error">ImageUrl is not valid!</p> */}
+    return (
+        <div className="create-book-modal">
+            <div className="book-container">
+                <header className="headers">
+                <button className="btn close"
+                        // onClick={onClose}
+                    > X </button>
+                    <h2>Nominate New Book</h2>
 
-            <label htmlFor="genre">Genre:</label>
-            <input type="text" id="genre" name="genre" />
-            {/* <p className="form-error">
-                            Genre should be at least 3 characters long!
-                            </p> */}
+                    <form onSubmit={createBookSubmitHandler}>
+                        <label htmlFor="title">Title:</label>
+                        <input type="text" id="title" name="title" />
+                        {/* <p className="form-error">
+                                        Title should be at least 3 characters long!
+                                        </p> */}
 
-            <label htmlFor="publishedAt">Published At:</label>
-            <input type="text" id="publishedAt" name="publishedAt" />
-            {/* <p className="form-error">
-                            Published At is not valid!!
-                                </p> */}
+                        <label htmlFor="author">Author:</label>
+                        <input type="text" id="author" name="author" />
+                        {/* <p className="form-error">
+                                        Author should be at least 3 characters long!
+                                        </p> */}
 
-            <label htmlFor="desc">Description:</label>
-            <textarea type="text" id="desc" name="desc" />
-            {/* <p className="form-error">Description should be at least 6 characters long!</p> */}
+                        <label htmlFor="imageUrl">Image URL:</label>
+                        <input type="text" id="imageUrl" name="imageUrl" />
+                        {/* <p className="form-error">ImageUrl is not valid!</p> */}
 
-            <div className="modal-buttons">
-              <button id="action-save" className="btn" type="submit">
-                Save
-              </button>
-              <button
-                id="action-cancel"
-                className="btn"
-                type="button"
-                // onClick={onClose}
-              >
-                Cancel
-              </button>
+                        <label htmlFor="genre">Genre:</label>
+                        <input type="text" id="genre" name="genre" />
+                        {/* <p className="form-error">
+                                        Genre should be at least 3 characters long!
+                                        </p> */}
+
+                        <label htmlFor="publishedAt">Published At:</label>
+                        <input type="text" id="publishedAt" name="publishedAt" />
+                        {/* <p className="form-error">
+                                        Published At is not valid!!
+                                            </p> */}
+
+                        <label htmlFor="desc">Description:</label>
+                        <textarea type="text" id="desc" name="desc" />
+                        {/* <p className="form-error">Description should be at least 6 characters long!</p> */}
+
+                        <div className="modal-buttons">
+                            <button id="action-save" className="btn" type="submit">
+                            Save
+                            </button>
+                            <button
+                            id="action-cancel"
+                            className="btn"
+                            type="button"
+                            // onClick={onClose}
+                            >
+                            Cancel
+                            </button>
+                        </div>
+                    </form>
+                </header>
             </div>
-          </form>
-        </header>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
