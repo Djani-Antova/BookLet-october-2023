8c1f8852ce81b72e3280ac6b00d2652ee13ab62c
djaniAntova <djaniantova@gmail.com>
2023-11-18 11:22:28 +0200

    Add FeaturedListItem component

---
+++ client/src/components/book-list/book-list-item/FeaturedListItem.jsx
@@ -0,0 +1,54 @@
+export default function FeaturedListItem ({
+    
+    title,
+    author,
+    imageUrl,
+    genre,
+    publishedAt,
+    desc
+}) {
+
+    return(
+        <article className="entry">
+        <div className="media">
+        <img
+            src={imageUrl}
+            alt={`${title}'s poster`}
+            style={{ width: "152px", height: "227px" }}
+        />
+        </div>
+        <section className="entry-content">
+        <div className="meta">
+            <div className="tags">
+            <label>{formatDate(publishedAt)}</label>
+            </div>
+            <p className="reading-time">{genre}</p>
+        </div>
+        <header className="entry-header">
+            <h4>{title}</h4>
+        </header>
+        <p className="short-desc">{desc}</p>
+        <p>
+            <a href="#" className="read-more">
+            Read more
+            </a>
+        </p>
+
+        <div className="actions">
+                <button className="btn edit-btn" title="Edit">
+                    Edit
+                </button>
+                <button className="btn delete-btn" title="Delete" >
+                    Delete
+                </button>
+                <button className="btn info-btn" title="Info" >
+                    Details
+                </button>
+        </div>
+
+
+        </section>
+    </article>
+);
+    
+}
