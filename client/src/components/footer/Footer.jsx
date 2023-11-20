45600ee7dc7b00d4b1ea6680c49892bef3c6f86f
djaniAntova <djaniantova@gmail.com>
2023-11-20 15:32:19 +0200

    Update Footer.jsx

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
-import "./Footer.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import "./Footer.css";
 
 export default function Footer() {
   const githubUrl = 'https://github.com/Djani-Antova';
