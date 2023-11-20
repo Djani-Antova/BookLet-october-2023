4370579d8a2a01e79a47ac78076c0d93ce068a68
djaniAntova <djaniantova@gmail.com>
2023-11-20 15:31:51 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/navigation/Navigation";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
-import FeaturedList from "./components/book-list/FeaturedList";
+import NominationsList from "./components/book-list/NominationsList";
 import CreateBookModal from "./components/book-create/CreateBookModal";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
@@ -15,7 +15,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/books" element={<FeaturedList />} />
+          <Route path="/books" element={<NominationsList />} />
           <Route path="/create" element={<CreateBookModal />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
