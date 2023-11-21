d60c9fd96028fe8d13be1ba4d4c3675ebc938b07
djaniAntova <djaniantova@gmail.com>
2023-11-22 01:59:18 +0200

    Add comments to BookDetails page

--- client/src/components/book-details/BookDetails.jsx
+++ client/src/components/book-details/BookDetails.jsx
@@ -1,24 +1,23 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-import * as bookService from "../../services/bookService"
+import * as bookService from "../../services/bookService";
 
 import "./BookDetails.css";
 
 function BookDetails() {
-    const [book, setBook] = useState({});
-    const { bookId } = useParams();
+  const [book, setBook] = useState({});
+  const { bookId } = useParams();
 
-    useEffect(() => {
-        bookService.getOne(bookId)
-            .then(setBook);
-    }, [bookId])
+  useEffect(() => {
+    bookService.getOne(bookId).then(setBook);
+  }, [bookId]);
 
   return (
-    <>    
+    <>
       <div className="wrapper">
         <div className="product-img">
-          <img src={book.imageUrl} height={420} width={327} alt={book.title}/>
+          <img src={book.imageUrl} height={420} width={327} alt={book.title} />
         </div>
         <div className="product-info">
           <div className="product-text">
@@ -28,6 +27,7 @@ function BookDetails() {
             <h6>Published at: {book.publishedAt}</h6>
             <p>{book.desc}</p>
           </div>
+
           <div className="product-btn">
             <button className="edit" type="button">
               Edit
@@ -37,7 +37,36 @@ function BookDetails() {
             </button>
           </div>
         </div>
+
+
       </div>
+
+      <div className="details-comments">
+        <h2>Comments:</h2>
+        <ul>
+          <li className="comment">
+            <p>Content: I rate this one quite highly.</p>
+          </li>
+          <li className="comment">
+            <p>
+              Content: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              imos similique sapiente?
+            </p>
+          </li>
+        </ul>
+        <p className="no-comment">No comments.</p>
+      </div>
+
+              <article className="create-comment">
+                <label>Add new comment:</label>
+                <form className="form" >
+                    <input type="text" name="username" placeholder="username" />
+                    <textarea name="comment" placeholder="Comment......"></textarea>
+                    <input className="btn submit" type="submit" value="Add Comment" />
+                </form>
+            </article>
+
+      
     </>
   );
 }
