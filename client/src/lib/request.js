c56b565be06727673c2b0f6ac8788b65ca8384b8
djaniAntova <djaniantova@gmail.com>
2023-11-21 16:11:15 +0200

    Modify requester

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,18 +1,19 @@
-// const buildOptions = (data) => {
-//     const options = {};
+const buildOptions = (data) => {
+    const options = {};
 
-//     if (data) {
-//         options.body = JSON.stringify(data);
-//         options.headers = {
-//             'content-type': 'application/json'
-//         };
-//     }
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers = {
+            'content-type': 'application/json'
+        };
+    }
 
-//     return options;
-// };
+    return options;
+};
 
-export const request = async (method, url, data) => {
+const request = async (method, url, data) => {
     const response = await fetch(url, {
+        ...buildOptions(data),
         method,
     });
 
@@ -21,8 +22,8 @@ export const request = async (method, url, data) => {
     return result;
 };
 
-// export const get = request.bind(null, 'GET');
-// export const post = request.bind(null, 'POST');
-// export const put = request.bind(null, 'PUT');
-// export const remove = request.bind(null, 'DELETE');
-// export const patch = request.bind(null, 'PATCH');
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const remove = request.bind(null, 'DELETE');
+export const patch = request.bind(null, 'PATCH');
--- client/src/services/bookService.js
+++ client/src/services/bookService.js
@@ -1,23 +1,15 @@
-import { request } from '../lib/request'
+import * as request from '../lib/request';
 
 const baseUrl = `http://localhost:3030/jsonstore/books`;
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl); 
+    const result = await request.get(baseUrl)
     
     return Object.values(result)
 };
 
 export const create = async (bookData) => {
-    const response = await fetch( baseUrl, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(bookData)
-    });
-
-    const result = await response.json();
-
+    const result = await request.post(baseUrl, bookData);
+    
     return result;
 }
