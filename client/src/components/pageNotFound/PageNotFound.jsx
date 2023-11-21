e0f0b35f0bcccbd49c1ed3a301966fe12087e3d1
djaniAntova <djaniantova@gmail.com>
2023-11-21 15:07:21 +0200

    Update PageNotFound.jsx

--- client/src/components/pageNotFound/PageNotFound.jsx
+++ client/src/components/pageNotFound/PageNotFound.jsx
@@ -3,10 +3,8 @@ import './PageNotFound.css';
 export default function PageNotFound() {
   return (
     <>
-    <div className="hit-the-floor">Page Not Found</div>
-    <div className="alternative-versions absolute bottom-0 w-full flex justify-center underline space-x-4 p-12">
-   
-    </div>
-  </>
+      <div className="hit-the-floor">Page Not Found</div>
+      <div className="alternative-versions absolute bottom-0 w-full flex justify-center underline space-x-4 p-12"></div>
+    </>
   );
 }
