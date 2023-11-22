a7bc9ce8783a3b98edb126fc864fe717f7731e83
djaniAntova <djaniantova@gmail.com>
2023-11-22 12:09:11 +0200

    Update commentService.js

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -2,10 +2,17 @@ import * as request from '../lib/request';
 
 const baseUrl = `http://localhost:3030/jsonstore/comments`;
 
-export const getAll = async () => {
-    const result = await request.get(baseUrl)
-    
-    return Object.values(result);
+export const getAll = async (bookId) => {
+
+    const query = new URLSearchParams({
+        where: `bookId="${bookId}"`
+    });
+
+
+    const result = await request.get(`${baseUrl}`);
+
+    // TODO: temp solution until migration to collections service 
+    return Object.values(result).filter(comment => comment.bookId === bookId);
 };
 
 export const create = async (bookId, username, text) => {
