7c08b018c3da1d773b1a4e76d7e333fd578b704e
djaniAntova <djaniantova@gmail.com>
2023-11-22 11:10:44 +0200

    Add new comment

--- client/src/components/book-details/BookDetails.jsx
+++ client/src/components/book-details/BookDetails.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import * as bookService from "../../services/bookService";
+import * as commentService from "../../services/commentService";
 
 import "./BookDetails.css";
 
@@ -13,8 +14,22 @@ function BookDetails() {
     bookService.getOne(bookId).then(setBook);
   }, [bookId]);
 
+  const addComentHandler = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const newComment = await commentService.create(
+        bookId,
+        formData.get('username'),
+        formData.get('comment')
+    );
+
+    console.log(newComment);
+  }
+
   return (
-    <>
+    <div id="details-wrapper">
       <div className="wrapper">
         <div className="product-img">
           <img src={book.imageUrl} height={420} width={327} alt={book.title} />
@@ -37,8 +52,6 @@ function BookDetails() {
             </button>
           </div>
         </div>
-
-
       </div>
 
       <div className="details-comments">
@@ -53,21 +66,25 @@ function BookDetails() {
               imos similique sapiente?
             </p>
           </li>
+          <li className="comment">
+            <p>
+              Content: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              imos similique sapiente?
+            </p>
+          </li>
         </ul>
         <p className="no-comment">No comments.</p>
       </div>
 
-              <article className="create-comment">
-                <label>Add new comment:</label>
-                <form className="form" >
-                    <input type="text" name="username" placeholder="username" />
-                    <textarea name="comment" placeholder="Comment......"></textarea>
-                    <input className="btn submit" type="submit" value="Add Comment" />
-                </form>
-            </article>
-
-      
-    </>
+      <article className="create-comment">
+        <label>Add new comment:</label>
+        <form className="form" onSubmit={addComentHandler}>
+          <input type="text" name="username" placeholder="username" />
+          <textarea name="comment" placeholder="Comment......"></textarea>
+          <input className="btn submit" type="submit" value="Add Comment" />
+        </form>
+      </article>
+    </div>
   );
 }
 
