986eba8219996df12262c7300ed149959a073a06
djaniAntova <djaniantova@gmail.com>
2023-11-23 13:46:20 +0200

    Update Home component and its styling

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -12,16 +12,14 @@ export default function Home() {
                             Welcome to <br />
                             BookLet Forum!
                         </h1>          
-                        <p>
-                            Welcome to our platform dedicated to celebrating the literary triumphs
-                            of the past decade. As we approach the end of this remarkable era, we
-                            invite you to join us in recognizing the books that have left an
-                            indelible mark on readers worldwide. From compelling narratives that
+                        <p className="home-paragraph">
+                            Welcome to our platform dedicated to nominating the literary triumphs
+                            for the Booker Prize. From compelling narratives that
                             ignited imaginations to thought-provoking non-fiction that reshaped
                             perspectives, these works have defined the literary landscape of the
-                            2010s.
+                            last decade.
                         </p>
-                        <p>
+                        <p className="home-paragraph">
                             Explore our curated selections and share your own nominations, as we
                             come together to honor the best books of the decade. Let us embark on
                             this journey of literary appreciation and pay tribute to the voices that
