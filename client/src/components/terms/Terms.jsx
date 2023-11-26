2d952fbf73a94aa0d317ba8d2cdc7cd76b042f75
djaniAntova <djaniantova@gmail.com>
2023-11-26 15:43:39 +0200

    Add terms of service page

---
+++ client/src/components/terms/Terms.jsx
@@ -0,0 +1,55 @@
+// Terms.jsx
+
+import React from 'react';
+import './Terms.css';
+
+export default function Terms() {
+  return (
+    <div className="terms-container">
+      <h2 className="about-section-heading">Terms of Service</h2>
+
+      <p>
+        By accessing and using the BookLet website, you agree to comply with
+        and be bound by the following terms and conditions. If you disagree
+        with any part of these terms, please do not use our services.
+      </p>
+
+      <h3>Acceptance of Terms</h3>
+      <p>
+        Your use of BookLet is subject to these Terms of Service. By accessing
+        our services, you agree to be bound by these terms and all applicable
+        laws and regulations.
+      </p>
+
+      <h3>User Accounts</h3>
+      <p>
+        To use certain features of BookLet, you may be required to create a user
+        account. You are responsible for maintaining the confidentiality of
+        your account information and agree to accept responsibility for all
+        activities that occur under your account.
+      </p>
+
+      <h3>Content</h3>
+      <p>
+        Users may contribute content to BookLet. By submitting content, you
+        grant us a non-exclusive, worldwide, royalty-free, irrevocable,
+        sub-licensable license to use, reproduce, modify, adapt, publish,
+        translate, create derivative works from, distribute, and display such
+        content.
+      </p>
+
+      <h3>Prohibited Activities</h3>
+      <p>
+        You agree not to engage in any activity that disrupts or interferes with
+        our services, violates applicable laws, or infringes on the rights of
+        others. Prohibited activities include but are not limited to
+        unauthorized access, spamming, and harassment.
+      </p>
+
+      <p>
+        If you have any questions or concerns about our Terms of Service, please
+        contact us at terms@booklet.com.
+      </p>
+    </div>
+  );
+}
