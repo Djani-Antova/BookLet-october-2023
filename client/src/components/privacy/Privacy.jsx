3915cab91d8df223587b958c2e60d85508ed9ee6
djaniAntova <djaniantova@gmail.com>
2023-11-30 11:16:48 +0200

    Implement minor changes

--- client/src/components/privacy/Privacy.jsx
+++ client/src/components/privacy/Privacy.jsx
@@ -4,50 +4,50 @@ import React from 'react';
 import './Privacy.css';
 
 export default function Privacy() {
-  return (
-    <div className="privacy-container">
-      <h2 className="about-section-heading">Privacy Policy</h2>
+    return (
+        <div className="privacy-container">
+            <h2 className="about-section-heading">Privacy Policy</h2>
 
-      <p>
-        At BookLet, we are committed to protecting your privacy and ensuring the
-        security of your personal information. This Privacy Policy outlines how
-        we collect, use, and disclose your information when you use our website
-        or services.
-      </p>
+            <p>
+            At BookLet, we are committed to protecting your privacy and ensuring the
+            security of your personal information. This Privacy Policy outlines how
+            we collect, use, and disclose your information when you use our website
+            or services.
+            </p>
 
-      <h3>Your Information</h3>
-      <p>
-        We may collect personal information such as your name, email address,
-        and other relevant details when you register or interact with our
-        services. This information is used to provide and improve our services
-        and to communicate with you.
-      </p>
+            <h3>Your Information</h3>
+            <p>
+            We may collect personal information such as your name, email address,
+            and other relevant details when you register or interact with our
+            services. This information is used to provide and improve our services
+            and to communicate with you.
+            </p>
 
-      <h3>Usage Data</h3>
-      <p>
-        We may collect information about how you interact with our website,
-        including your IP address, browser type, and pages visited. This data is
-        used for analytics and to improve our services.
-      </p>
+            <h3>Usage Data</h3>
+            <p>
+            We may collect information about how you interact with our website,
+            including your IP address, browser type, and pages visited. This data is
+            used for analytics and to improve our services.
+            </p>
 
-      <h3>Security</h3>
-      <p>
-        We take reasonable measures to protect your information from
-        unauthorized access or disclosure. However, no method of transmission
-        over the internet or electronic storage is completely secure.
-      </p>
+            <h3>Security</h3>
+            <p>
+            We take reasonable measures to protect your information from
+            unauthorized access or disclosure. However, no method of transmission
+            over the internet or electronic storage is completely secure.
+            </p>
 
-      <h3>Third-Party Links</h3>
-      <p>
-        Our website may contain links to third-party websites. We are not
-        responsible for the privacy practices or content of these websites.
-        Please review the privacy policies of those third parties.
-      </p>
+            <h3>Third-Party Links</h3>
+            <p>
+            Our website may contain links to third-party websites. We are not
+            responsible for the privacy practices or content of these websites.
+            Please review the privacy policies of those third parties.
+            </p>
 
-      <p>
-        If you have any questions or concerns about our Privacy Policy, please
-        contact us at privacy@booklet.com.
-      </p>
-    </div>
-  );
+            <p>
+            If you have any questions or concerns about our Privacy Policy, please
+            contact us at privacy@booklet.com.
+            </p>
+        </div>
+    );
 }
--- client/src/services/bookService.js
+++ client/src/services/bookService.js
@@ -20,4 +20,12 @@ export const create = async (bookData) => {
     const result = await request.post(baseUrl, bookData);
     
     return result;
-}
+};
+
+export const edit = async (bookId, bookData) => {
+    const result = await request.put(`${baseUrl}/${bookId}`, bookData);
+
+    return result;
+};
+
+export const remove = async (bookId) => request.remove(`${baseUrl}/${bookId}`)
