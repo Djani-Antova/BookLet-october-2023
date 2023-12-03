011731be896022723d4e64971d4b9246f2dd526b
djaniAntova <djaniantova@gmail.com>
2023-12-03 16:11:45 +0200

    Update details

--- client/src/components/book-details/BookDetails.jsx
+++ client/src/components/book-details/BookDetails.jsx
@@ -1,14 +1,10 @@
+import React, { useState, useEffect, useContext } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import { useContext, useEffect, useMemo, useState } from "react";
-
 import * as bookService from "../../services/bookService";
 import * as commentService from "../../services/commentService";
 import AuthContext from "../../contexts/authContext";
-import useForm from "../hooks/useForm";
 import Path from "../../paths";
 import { runSuccessfulBookDeletion, runEmptyFieldAlert } from '../../utils/alerts';
-
-
 import "./BookDetails.css";
 import { pathToUrl } from "../../utils/pathUtils";
 
@@ -17,7 +13,7 @@ function BookDetails() {
   const [book, setBook] = useState({});
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const { bookId } = useParams();
 
@@ -26,104 +22,100 @@ function BookDetails() {
     commentService.getAll(bookId).then(setComments);
   }, [bookId]);
 
-  const addComentHandler = async (values) => {
+  const addCommentHandler = async (e) => {
+    e.preventDefault();
+
     // Check if the comment is empty
-    if (!values.comment.trim()) {
-        runEmptyFieldAlert();
-        return;
+    if (!newComment.trim()) {
+      runEmptyFieldAlert();
+      return;
     }
-  
-    const createdComment = await commentService.create(
-        bookId,
-        values.comment
-    );
-  
+
+    const createdComment = await commentService.create(bookId, newComment);
+
     setComments((state) => [
-        ...state,
-        { ...createdComment, owner: { username } },
+      ...state,
+      { ...createdComment, owner: { username } },
     ]);
 
     setNewComment(""); // Reset the new comment state to clear the textarea
   };
 
-    const deleteBookHandler = async (e) => {
-        e.preventDefault(); // Prevent the default link behavior
-        
-        const hasConfirmed = confirm(`Are you sure you want to delete ${book.title}`)
+  const deleteBookHandler = async (e) => {
+    e.preventDefault();
 
-        if(hasConfirmed) {
-            await bookService.remove(bookId);
-            runSuccessfulBookDeletion();
-            navigate('/books');
-        } else {
-            navigate('/books');
-        }
+    const hasConfirmed = window.confirm(`Are you sure you want to delete ${book.title}`);
+
+    if (hasConfirmed) {
+      await bookService.remove(bookId);
+      runSuccessfulBookDeletion();
+      navigate('/books');
+    } else {
+      navigate('/books');
+    }
   };
-
-    //TODO temp solution for form reinitialization
-    const initialValues = useMemo(() => ({
-    comment: "",
-    }), [])
-
-  const { values, onChange, onSubmit } = useForm(addComentHandler, initialValues);
 
   const isOwner = userId === book._ownerId;
 
-    return (
-        <div id="details-wrapper">
-        <div className="wrapper">
+  return (
+    <div id="details-wrapper">
+      <div className="wrapper">
         <div className="product-img">
-            <img src={book.imageUrl} height={420} width={327} alt={book.title} />
+          <img src={book.imageUrl} height={420} width={327} alt={book.title} />
         </div>
         <div className="product-info">
-            <div className="product-text">
+          <div className="product-text">
             <h1>{book.title}</h1>
             <h2>By: {book.author}</h2>
             <h5>Genre: {book.genre}</h5>
             <h6>Published at: {book.publishedAt}</h6>
             <p>{book.desc}</p>
-            </div>
+          </div>
 
-            {isOwner && (
+          {isOwner && (
             <div className="product-btn">
-                <Link to={pathToUrl(Path.BookEdit, { bookId })} className="edit" type="button">Edit </Link>
-                <Link to={Path.BookDelete} className="delete" type="button" onClick={deleteBookHandler}> Delete </Link>
+              <Link to={pathToUrl(Path.BookEdit, { bookId })} className="edit" type="button">Edit </Link>
+              <button className="delete" type="button" onClick={deleteBookHandler}> Delete </button>
             </div>
-            )}
+          )}
         </div>
-        </div>
+      </div>
 
-        <div className="details-comments">
+      <div className="details-comments">
         <h2>Comments:</h2>
         <ul>
-            {comments.map(({ _id, text, owner: { username } }) => (
+          {comments.map(({ _id, text, owner: { username } }) => (
             <li key={_id} className="comment">
-                <p>
+              <p>
                 {username}: {text}
-                </p>
+              </p>
             </li>
-            ))}
+          ))}
         </ul>
 
         {comments.length === 0 && <p className="no-comment">No comments.</p>}
 
         {isAuthenticated && (
-            <article className="create-comment">
+          <article className="create-comment">
             <label>Add new comment:</label>
-            <form className="form" onSubmit={onSubmit}>
-                <textarea
+            <form className="form" onSubmit={addCommentHandler}>
+              <textarea
                 name="comment"
                 placeholder="Comment......"
-                value={values.comment}
-                onChange={onChange}
-                ></textarea>
-                <input className="btn submit" type="submit" value="Add Comment" />
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+              ></textarea>
+                  <p>
+                    <button type="submit" className="btn submit">
+                        Submit
+                    </button>
+                  </p>
             </form>
-            </article>
+          </article>
         )}
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default BookDetails;
