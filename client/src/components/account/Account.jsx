6dabc631ee96080e49e18d55e5b0146f795126e5
djaniAntova <djaniantova@gmail.com>
2023-12-03 20:21:10 +0200

    Add error handling to user's account

--- client/src/components/account/Account.jsx
+++ client/src/components/account/Account.jsx
@@ -4,15 +4,18 @@ import AuthContext from '../../contexts/authContext.jsx'
 import MyBook from './MyBook';
 
 export default function Account() {
-    const { userId } = useContext(AuthContext)
+    const { userId } = useContext(AuthContext);
     const [myBooks, setMyBooks] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        if (userId) {
-            // Call getMyBooks only if userId is available
+        if (userId) {          
             bookService.getMyBooks(userId)
                 .then((result) => setMyBooks(result))
-                .catch(err => console.log(err));
+                .catch((err) => {
+                    console.error("Error fetching my books:", err);
+                    setError('Error fetching my books. Please try again later.');
+                });
         }
     }, [userId]); 
 
@@ -20,8 +23,17 @@ export default function Account() {
         <section className="section-blog featured section">
             <div className="section-container">
                 <h2>My Nominations For <br /> <span>BookLet Award</span> </h2>
-                {myBooks.map(book => <MyBook key={book._id} {...book} />)}
-                {!myBooks.length &&  <h4> You have no nominations yet.</h4> }
+
+                {error ? (
+                    <div style={{ color: 'red', fontSize: '2em' }}>
+                        {error}
+                    </div>
+                ) : (
+                    <>
+                        {myBooks.map(book => <MyBook key={book._id} {...book} />)}
+                        {!myBooks.length &&  <h4> You have no nominations yet.</h4>}
+                    </>
+                )}
             </div>
         </section>
     );
