9083cafc2f2b7eb36e73345b8c23f48c1fd1221d
djaniAntova <djaniantova@gmail.com>
2023-12-03 18:04:42 +0200

    Add error handling to recent list

--- client/src/components/latest/Latest.jsx
+++ client/src/components/latest/Latest.jsx
@@ -1,36 +1,39 @@
 import { useEffect, useState } from 'react';
-
-import * as bookService from '../../services/bookService.js'
+import * as bookService from '../../services/bookService.js';
 import LatestBook from './LatestBook.jsx';
-
-
 
 import './Latest.css';
 
 export default function Latest() {
-
     const [latestBooks, setLatestBooks] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         bookService.getLatest()
-            .then((result) => setLatestBooks(result))
-            .catch(err => console.log(err))     // TODO da dobavim user action, user notification     
+            .then((result) => {
+                setLatestBooks(result);
+                setError(null); // Clear any previous errors
+            })
+            .catch((err) => {
+                console.error(err); // Log the error for debugging
+                setError('Error fetching latest books. Please try again later.');
+            });
     }, []); 
-    console.log(latestBooks);
-    
-   
-    return (
-        
-            <section className="section-blog featured section">
-                <div className="section-container">
-                    <h2>Latest Three Nominations For <br /> <span>BookLet Award</span> </h2>                
 
-                    {latestBooks.map(book => <LatestBook key={book._id} {...book} />)}
-                
-                    {!latestBooks.length &&  <h4> There are no nominations yet.</h4> }
-                
-                </div>
-            </section>
-     
+    return (
+        <section className="section-blog featured section">
+            <div className="section-container">
+                <h2>Latest Three Nominations For <br /> <span>BookLet Award</span> </h2>                
+
+                {error ? (
+                    <div style={{ border: '4px solid red', fontSize: '2em', color: 'bisque', padding: '25px' }}>{error}</div>
+                ) : (
+                    <>
+                        {latestBooks.map(book => <LatestBook key={book._id} {...book} />)}
+                        {!latestBooks.length && <h4> There are no nominations yet.</h4>}
+                    </>
+                )}
+            </div>
+        </section>
     );
 }
