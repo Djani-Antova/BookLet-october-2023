5f5f054f6b152d5d3f430e0a27dbd960f4c20883
djaniAntova <djaniantova@gmail.com>
2023-12-29 19:45:23 +0200

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,11 @@
 import * as request from '../lib/request';
 
-const baseUrl = `http://localhost:3030/users`;
+// const baseUrl = `http://localhost:3030/users`;
+
+// Use environment variable for baseUrl
+const baseUrl = `${import.meta.env.VITE_API_URL}/users` || `http://localhost:3030/users`;
+
+
 
 export const login = async (email, password) => {
     const result = await request.post(`${baseUrl}/login`, {
